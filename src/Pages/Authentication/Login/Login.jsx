a26ea92f188718b82b93676a9391866bc5b38b5f
ramjan88 <gmramjanali8888@gmail.com>
2025-07-09 14:33:06 +0600

    update tailwind color theme

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -10,32 +10,32 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#0f0f0f] px-4">
+    <div className="min-h-screen flex items-center justify-center bg-base-content px-4">
       <motion.div
         initial={{ opacity: 0, x: -100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 2 }}
-        className="backdrop-blur-md bg-[#0f0f0f]/60 border border-cyan-400/30 shadow-[0_0_20px_rgba(0,255,255,0.2)] rounded-2xl w-full max-w-5xl flex flex-col-reverse md:flex-row overflow-hidden"
+        className="backdrop-blur-md bg-base-content/60 border border-primary/30 shadow-[0_0_20px_rgba(0,255,255,0.2)] rounded-2xl w-full max-w-5xl flex flex-col-reverse md:flex-row overflow-hidden"
       >
         {/* Left - Login Form */}
-        <div className="w-full md:w-1/2 p-10 flex flex-col justify-center text-white">
-          <h2 className="text-3xl font-bold text-cyan-400 text-center mb-8">
+        <div className="w-full md:w-1/2 p-10 flex flex-col justify-center text-secondary-content">
+          <h2 className="text-3xl font-bold text-primary text-center mb-8">
             Login
           </h2>
 
           <form className="space-y-6">
             {/* Username */}
             <div className="group relative">
-              <label className="block text-sm mb-2 text-gray-300">
+              <label className="block text-sm mb-2 text-primary-content">
                 Username
               </label>
-              <div className="flex items-center border-b border-gray-600 group-focus-within:border-cyan-400 transition-all duration-300 px-2">
+              <div className="flex items-center border-b border-primary-content group-focus-within:border-primary transition-all duration-300 px-2">
                 <input
                   type="text"
                   placeholder="Enter username"
-                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-white placeholder-gray-500"
+                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-secondary-content placeholder-primary-content"
                 />
-                <span className="text-gray-400 pr-2">
+                <span className="text-primary-content pr-2">
                   <FaUser />
                 </span>
               </div>
@@ -43,19 +43,19 @@ const Login = () => {
 
             {/* Password */}
             <div className="group relative">
-              <label className="block text-sm mb-2 text-gray-300">
+              <label className="block text-sm mb-2 text-primary-content">
                 Password
               </label>
-              <div className="flex items-center border-b border-gray-600 group-focus-within:border-cyan-400 transition-all duration-300 px-2">
+              <div className="flex items-center border-b border-primary-content group-focus-within:border-primary transition-all duration-300 px-2">
                 <input
                   type={showPassword ? "text" : "password"}
                   placeholder="Enter password"
-                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-white placeholder-gray-500"
+                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-secondary-content placeholder-primary-content"
                 />
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="text-gray-400 pr-2 focus:outline-none cursor-pointer"
+                  className="text-primary-content pr-2 focus:outline-none cursor-pointer"
                 >
                   {showPassword ? <IoIosEyeOff /> : <IoIosEye />}
                 </button>
@@ -63,19 +63,19 @@ const Login = () => {
             </div>
 
             {/* Login Button */}
-            <motion.button
-              whileHover={{ scale: 1.05 }}
-              className="w-full py-2 mt-4 rounded-full bg-gradient-to-r from-cyan-400 to-blue-500 text-white font-semibold shadow-lg hover:from-blue-500 hover:to-cyan-400 transition-all"
+            <button
+              
+              className="w-full py-2 mt-4 rounded-full bg-gradient-to-r from-primary to-secondary text-secondary-content font-semibold shadow-lg hover:from-secondary hover:to-primary transition-all cursor-pointer"
             >
               Login
-            </motion.button>
+            </button>
 
             {/* Sign Up */}
-            <p className="text-sm text-gray-400 text-center">
+            <p className="text-sm text-primary-content text-center">
               Don't have an account?{" "}
               <Link
                 to="/auth/register"
-                className="text-cyan-400 hover:underline"
+                className="text-primary hover:underline"
               >
                 Sign Up
               </Link>
@@ -89,7 +89,7 @@ const Login = () => {
           initial={{ opacity: 0, x: 100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 2 }}
-          className="w-full md:w-1/2 bg-gradient-to-tr from-[#00c6ff] to-[#0072ff] text-white p-10"
+          className="w-full md:w-1/2 border-r-1 border-primary/30 shadow-[0_0_20px_rgba(0,255,255,0.2)] text-secondary-content p-10"
         >
           <div className="">
             <TechLoomaLogo></TechLoomaLogo>
--- src/Pages/Authentication/Register/Register.jsx
+++ src/Pages/Authentication/Register/Register.jsx
@@ -10,31 +10,31 @@ const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#0f0f0f] px-4 py-10">
+    <div className="min-h-screen flex items-center justify-center bg-base-content px-4 py-10">
       <motion.div
         initial={{ opacity: 0, x: 100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 2 }}
-        className="backdrop-blur-md bg-[#0f0f0f]/60 border border-cyan-400/30 shadow-[0_0_20px_rgba(0,255,255,0.2)] rounded-2xl w-full max-w-5xl flex flex-col-reverse md:flex-row-reverse overflow-hidden"
+        className="backdrop-blur-md bg-base-content/60 border border-primary/30 shadow-[0_0_20px_rgba(0,255,255,0.2)] rounded-2xl w-full max-w-5xl flex flex-col-reverse md:flex-row-reverse overflow-hidden"
       >
         {/* Left - Register Form */}
-        <div className="w-full md:w-1/2 p-10 flex flex-col justify-center text-white">
-          <h2 className="text-3xl font-bold text-cyan-400 text-center mb-8">
+        <div className="w-full md:w-1/2 p-10 flex flex-col justify-center text-secondary-content">
+          <h2 className="text-3xl font-bold text-primary text-center mb-8">
             Register
           </h2>
 
           <form className="space-y-6">
             {/* Name */}
             <div className="group relative">
-              <label className="block text-sm mb-2 text-gray-300">Name</label>
-              <div className="flex items-center border-b border-gray-600 px-2 group-focus-within:border-cyan-400 transition-all duration-300">
+              <label className="block text-sm mb-2 text-primary-content">Name</label>
+              <div className="flex items-center border-b border-primary-content px-2 group-focus-within:border-primary transition-all duration-300">
                 <input
                   type="text"
                   placeholder="Your full name"
-                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-white placeholder-gray-500"
+                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-secondary-content placeholder-primary-content"
                   required
                 />
-                <span className="text-gray-400 pr-2">
+                <span className="text-primary-content pr-2">
                   <FaUser />
                 </span>
               </div>
@@ -42,15 +42,15 @@ const Register = () => {
 
             {/* Email */}
             <div className="group relative">
-              <label className="block text-sm mb-2 text-gray-300">Email</label>
-              <div className="flex items-center border-b border-gray-600 px-2 group-focus-within:border-cyan-400 transition-all duration-300">
+              <label className="block text-sm mb-2 text-primary-content">Email</label>
+              <div className="flex items-center border-b border-primary-content px-2 group-focus-within:border-primary transition-all duration-300">
                 <input
                   type="email"
                   placeholder="Enter Email"
-                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-white placeholder-gray-500"
+                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-secondary-content placeholder-primary-content"
                   required
                 />
-                <span className="text-gray-400 pr-2">
+                <span className="text-primary-content pr-2">
                   <FaEnvelope />
                 </span>
               </div>
@@ -58,59 +58,56 @@ const Register = () => {
 
             {/* Password */}
             <div className="group relative">
-              <label className="block text-sm mb-2 text-gray-300">
+              <label className="block text-sm mb-2 text-primary-content">
                 Password
               </label>
-              <div className="flex items-center border-b border-gray-600 px-2 group-focus-within:border-cyan-400 transition-all duration-300">
+              <div className="flex items-center border-b border-primary-content px-2 group-focus-within:border-primary transition-all duration-300">
                 <input
                   type={showPassword ? "text" : "password"}
                   placeholder="Enter password"
-                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-white placeholder-gray-500"
+                  className="bg-transparent w-full py-2 px-2 focus:outline-none text-secondary-content placeholder-primary-content"
                   required
                 />
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="text-gray-400 pr-2 focus:outline-none cursor-pointer"
+                  className="text-primary-content pr-2 focus:outline-none cursor-pointer"
                 >
                   {showPassword ? <IoIosEyeOff /> : <IoIosEye />}
                 </button>
               </div>
             </div>
 
-            {/* Photo URL */}
+            {/* Photo URL file*/}
             <div className="group relative">
-              <label className="block text-sm mb-2 text-gray-300">
+              <label className="block text-sm mb-2 text-primary-content">
                 Upload Profile Photo
               </label>
-              <div className="flex items-center border-b border-gray-600 px-2 py-2 group-focus-within:border-cyan-400 transition-all duration-300">
+              <div className="flex items-center border-b border-primary-content px-2 py-2 group-focus-within:border-primary transition-all duration-300">
                 <input
                   type="file"
                   accept="image/*"
-                  className="w-full text-gray-400 file:mr-4 file:py-1 file:px-3 
+                  className="w-full text-primary-content file:mr-4 file:py-1 file:px-3 
                  file:rounded-full file:border-0 file:text-sm 
-                 file:font-semibold file:bg-cyan-400 file:text-black 
-                 hover:file:bg-cyan-500"
+                 file:font-semibold file:bg-primary file:text-black file:cursor-pointer
+                 "
                   required
                 />
-                <span className="text-gray-400 pr-2">
+                <span className="text-primary-content pr-2">
                   <FaImage />
                 </span>
               </div>
             </div>
 
             {/* Register Button */}
-            <motion.button
-              whileHover={{ scale: 1.05 }}
-              className="w-full py-2 mt-4 rounded-full bg-gradient-to-r from-cyan-400 to-blue-500 text-white font-semibold shadow-lg hover:from-blue-500 hover:to-cyan-400 transition-all"
-            >
+            <button className="w-full py-2 mt-4 rounded-full bg-gradient-to-r from-primary to-secondary text-secondary-content font-semibold shadow-lg hover:from-secondary hover:to-primary transition-all cursor-pointer">
               Register
-            </motion.button>
+            </button>
 
             {/* Login Link */}
-            <p className="text-sm text-gray-400 text-center">
+            <p className="text-sm text-secondary-content text-center">
               Already have an account?{" "}
-              <Link to="/auth/login" className="text-cyan-400 hover:underline">
+              <Link to="/auth/login" className="text-primary hover:underline">
                 Sign in
               </Link>
             </p>
@@ -122,7 +119,7 @@ const Register = () => {
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: 0.3, duration: 2 }}
-          className="w-full md:w-1/2 bg-gradient-to-tr from-[#00c6ff] to-[#0072ff] text-white p-10 "
+          className="w-full md:w-1/2 border-r-1 border-primary/30 shadow-[0_0_20px_rgba(0,255,255,0.2)] text-secondary-content p-10 "
         >
           <div className="">
             <TechLoomaLogo></TechLoomaLogo>
